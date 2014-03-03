3b880329787095f50a2292031f2c96d50f07fba5
Ross Duggan <rduggan@engineyard.com>
2014-03-03 14:27:59 +0000

    Add tests and cleanup

--- update.sh
+++ update.sh
@@ -9,15 +9,26 @@ else
 	printf "Run from repo.\n"
 fi
 
+errexit() {
+	echo "$1 required"
+	exit 1
+}
+
+# Check available commands
+for cmd in php curl git pushd popd
+do
+	command -v $cmd > /dev/null || errexit $cmd
+done
 
 hash_current=$(php $current --version | cut -d' ' -f3 | cut -b 6-12)
 hash_new=$(curl -s -L https://getcomposer.org/version | cut -b 1-7)
 
 if [ $hash_current != $hash_new ]; then
 
-	loc=$(mktemp -t composer)
+	printf "New composer found, downloading...\n"
+	loc=$(mktemp -d -t composer) && mkdir -p $loc
 	curl -s -L https://getcomposer.org/composer.phar \
-		-o $loc
+		-o $loc/composer.phar
 	
 	changelog="debian/changelog"
 	tempfile=$(mktemp -t debcontrol)
@@ -25,11 +36,22 @@ if [ $hash_current != $hash_new ]; then
 	current_date="$(date +"%Y%m%d")"
 	rfc2822_date=$(date +"%a, %d %b %Y %T %z")
 	version="1.0.0-dev-$current_date"
-
-
 	composer_ver=$hash_new
+
+	printf "Testing..."
+	echo '{
+    "require": {
+        "monolog/monolog": "1.0.*"
+    }
+}' > $loc/composer.json
+	cwd=`pwd`
+	cd $loc
+	php composer.phar install > $loc/build.log || exit 1
+	cd $cwd
+	printf "OK.\n"
+
 	printf "Updating..."
-	mv $loc $current
+	mv $loc/composer.phar $current
 	echo "php-composer ($version-0+precise1) precise; urgency=low
 
   * Upstream release $version ($composer_ver).
@@ -40,6 +62,7 @@ if [ $hash_current != $hash_new ]; then
 	"$changelog" > "$tempfile" && mv "$tempfile" "$changelog" && \
 	git commit -m "$version (upstream $composer_ver)" debian composer.phar && \
 	git tag -a $version -m $version
+	rm -rf $loc
 	printf "Ready to push.\n"
 else
 	printf "No changes since last update.\n"
